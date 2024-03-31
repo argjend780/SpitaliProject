253bd10b30c0f5e6e2ca1f808cf5fa6ac2be5761
argjend780 <am67266@ubt-uni.net>
2024-03-31 16:26:38 +0200

    nav

--- demos-app-create/src/App.js
+++ demos-app-create/src/App.js
@@ -1,12 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { Navbar , Nav ,Container} from 'react-bootstrap';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
+import Login from './components/Login';
+import Regjister from './components/Regjister';
+import Home from './components/Home';
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      
+     <>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Spitali Amerikan</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link as={Link } to="/regjister">Regjister</Nav.Link>
+            <Nav.Link as={Link } to="/login">Login</Nav.Link>
+            <Nav.Link as={Link } to="/">Home</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+      <div>
+        <Routes>
+          <Route path='/login' element={<Login/>}/> 
+          <Route path='/regjister' element={<Regjister/>}/>
+          <Route path='/home' element={<Home/>}/>
+
+        </Routes>
+      </div>
     </div>
+    </BrowserRouter>
+
   );
 }
 
 export default App;
+
---
+++ demos-app-create/src/components/Regjister.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Regjister() {
+  return (
+    <div>Hello Regjister</div>
+  )
+}
+
+export default Regjister
